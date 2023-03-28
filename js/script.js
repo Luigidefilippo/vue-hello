ef06513dc339d2835d37296a37548f8831635a2a
Luigidefilippo <defilippoluigi65@gmail.com>
2023-03-28 13:42:13 +0200

    compleate exercise

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+
+
+
 const { createApp } = Vue;
 
-createRoot({
-  data() {
-    return {
-    };
-  },
-  
-}).mount("#root");
+createApp({
+    data() {
+        return {
+            message: "Hello VueJs!",
+            imgSrc: "https://d585tldpucybw.cloudfront.net/sfimages/default-source/default-album/870x220_telerik-blogsce2fdf065dfe46a2b2054ccc57d2d18d.png?sfvrsn=222ff053_1",
+            imgAlt: "immagine di Vue JS"
+        }
+    }
+}).mount("#app");
