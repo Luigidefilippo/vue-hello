3dfe3cb572b191ed2b3d150c6e28db0b27eabf50
Luigidefilippo <defilippoluigi65@gmail.com>
2023-03-30 17:33:43 +0200

    Fix vue hello

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Hello VueJs!",
-            imgSrc: "https://d585tldpucybw.cloudfront.net/sfimages/default-source/default-album/870x220_telerik-blogsce2fdf065dfe46a2b2054ccc57d2d18d.png?sfvrsn=222ff053_1",
-            imgAlt: "immagine di Vue JS"
+            text: "Hello Vuejs!",
+            imgSrc: "https://cdn.freecodecamp.org/curriculum/cat-photo-app/cats.jpg",
+            imgAlt: "immagine gatti "
         }
     }
 }).mount("#app");
